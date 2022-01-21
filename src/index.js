f1205c6800a49c59c94711f5306bd094892bed2f
Lajos Meszaros <m_lajos@hotmail.com>
2022-01-21 18:47:42 +0100

    Moved requires outside the main function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
+const unflatten = require('flat').unflatten;
+const flatten = require('flat');
+
+function isNumber(x) {
+    if (typeof x === 'number') return true;
+    if (/^0x[0-9a-f]+$/i.test(x)) {
+        try {
+            return Number(x) <= Number.MAX_SAFE_INTEGER;
+        } catch {
+            return false;
+        }
+    }
+    return /^[-+]?(?:\d+(?:\.\d*)?|\.\d+)(e[-+]?\d+)?$/.test(x);
+}
+
 module.exports = function (args, opts) {
-    var unflatten = require('flat').unflatten;
-    var flatten = require('flat');
     if (!opts) opts = {};
 
     var flags = {
@@ -257,15 +270,3 @@ module.exports = function (args, opts) {
 
     return unflatten(argv);
 };
-
-function isNumber(x) {
-    if (typeof x === 'number') return true;
-    if (/^0x[0-9a-f]+$/i.test(x)) {
-        try {
-            return Number(x) <= Number.MAX_SAFE_INTEGER;
-        } catch {
-            return false;
-        }
-    }
-    return /^[-+]?(?:\d+(?:\.\d*)?|\.\d+)(e[-+]?\d+)?$/.test(x);
-}
