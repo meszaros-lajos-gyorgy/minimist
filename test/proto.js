1ab79b3fed306ab5f6f135d1db8d0ab012df7a68
Micael Levi (@micalevisk) <mllc@icomp.ufam.edu.br>
2022-03-21 16:16:26 -0400

    test: add more two proto pollution tests
    Taken from https://github.com/substack/minimist/pull/165

--- test/proto.js
+++ test/proto.js
@@ -42,3 +42,19 @@ test('proto pollution (constructor)', function (t) {
   t.equal(argv.y, undefined);
   t.end();
 });
+
+test('proto pollution via _.concat constructor', function (t) {
+  var argv = parse(['--_.concat.constructor.prototype.y', '123']);
+  function fnToBeTested() {}
+  t.equal(fnToBeTested.y, undefined);
+  t.equal(argv.y, undefined);
+  t.end();
+});
+
+// Powered by Snyk: https://github.com/backstage/backstage/issues/10343
+test('proto pollution (constructor function) snyk', function (t) {
+  var argv = parse('--_.constructor.constructor.prototype.foo bar'.split(' '));
+  t.equal(function () {}.foo, undefined);
+  t.equal(argv.y, undefined);
+  t.end();
+});
